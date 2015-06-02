507fe8ba38124c71b2c4c4dfe38c5b2679891d17
Arunvel Sriram <arunvelsriram@gmail.com>
2015-06-03 02:14:41 +0530

    Added a proper welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,11 +2,21 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    public BibliotecaApp() {
+        System.out.println(welcomeMessage());
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
     }
 
     public String welcomeMessage() {
-        return "";
+        StringBuilder sb = new StringBuilder();
+        sb.append("Welcome to Biblioteca - A Library Management System\n");
+        int length = sb.length();
+        for (int i = 0; i < length; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
     }
 }
