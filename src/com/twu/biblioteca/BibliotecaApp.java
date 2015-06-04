1e9e70351708e88d84bde1a969c0a957eb942fc0
Arunvel Sriram <arunvelsriram@gmail.com>
2015-06-04 17:06:36 +0530

    Revert "Added a proper welcome message"
    This reverts commit 507fe8ba38124c71b2c4c4dfe38c5b2679891d17.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,21 +2,11 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
-    public BibliotecaApp() {
-        System.out.println(welcomeMessage());
-    }
-
     public static void main(String[] args) {
-        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        System.out.println("Hello, world!");
     }
 
     public String welcomeMessage() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Welcome to Biblioteca - A Library Management System\n");
-        int length = sb.length();
-        for (int i = 0; i < length; i++) {
-            sb.append("-");
-        }
-        return sb.toString();
+        return "";
     }
 }
