9faf774b3c747666785de20d43fc348d81c56211
Arunvel Sriram <arunvelsriram@gmail.com>
2015-06-05 01:41:58 +0530

    Change books data structure in BibliotecaApp

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -4,16 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BibliotecaApp {
-    List<String> books;
+    List<String[]> books;
 
     public BibliotecaApp() {
         books = new ArrayList<>();
-        books.add("Harry Potter and The Sorcer\'s Stone");
-        books.add("Harry Potter and The Chamber of Secrets");
-    }
-
-    public static void main(String[] args) {
-
+        books.add(new String[]{"Harry Potter and The Sorcer\'s Stone", "J. K. Rowling", "2000"});
+        books.add(new String[]{"Harry Potter and The Chamber of Secrets", "J. K. Rowling", "2001"});
     }
 
     public void welcomeMessage() {
@@ -21,8 +17,16 @@ public class BibliotecaApp {
     }
 
     public void books() {
-        for (int i = 0; i < books.size(); i++) {
-            System.out.println(i + 1 + ". " + books.get(i));
+        StringBuilder sb = new StringBuilder();
+        for (String[] book : books) {
+            sb.append(book[0]).append(", ").append(book[1]).append(", ").append(book[2]).append("\n");
         }
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.welcomeMessage();
+        bibliotecaApp.books();
     }
 }
