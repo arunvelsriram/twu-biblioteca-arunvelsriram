a96aed97ae5771cd8f2346f9f9003754ea0e4b5b
Arunvel Sriram <arunvelsriram@gmail.com>
2015-06-04 23:11:01 +0530

    Passed spec for welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,10 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+    }
+
+    public void welcomeMessage() {
+        System.out.println("Welcome to Biblioteca");
     }
 }
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,36 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class BibliotecaAppTest {
+    private ByteArrayOutputStream outputStream;
+
+    @Before
+    public void setUp() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @Test
+    public void shouldDisplayWelcomeMessageOnAppStart() {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.welcomeMessage();
+
+        String actual = outputStream.toString();
+
+        assertThat(actual, is("Welcome to Biblioteca\n"));
+    }
+
+    @After
+    public void clean() {
+        System.setOut(null);
+    }
+}
