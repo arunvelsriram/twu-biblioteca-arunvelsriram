066a13c0fe96665d79987563fbbde18b65935b9b
Arunvel Sriram <arunvelsriram@gmail.com>
2015-06-06 00:05:45 +0530

    Passed test for displaying welcome message

---
+++ src/com/twu/biblioteca/OutputHandler.java
@@ -0,0 +1,8 @@
+package com.twu.biblioteca;
+
+public class OutputHandler {
+
+    public void welcomeMessage() {
+        System.out.println("Welcome to Biblioteca");
+    }
+}
---
+++ test/com/twu/biblioteca/OutputHandlerTest.java
@@ -0,0 +1,38 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+
+public class OutputHandlerTest {
+    private ByteArrayOutputStream byteArrayOutputStream;
+
+    @Before
+    public void setUp() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+    }
+
+    @Test
+    public void shouldBeAbleToDisplayWelcomeMessage() {
+        OutputHandler outputHandler = new OutputHandler();
+        outputHandler.welcomeMessage();
+
+        String actualMessage = byteArrayOutputStream.toString();
+
+        assertThat(actualMessage, is(equalTo("Welcome to Biblioteca\n")));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(null);
+    }
+}
