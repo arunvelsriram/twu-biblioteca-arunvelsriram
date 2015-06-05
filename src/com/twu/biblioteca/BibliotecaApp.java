1476fec3ccc464c1c6188589f2a871efb5ab53b0
Arunvel Sriram <arunvelsriram@gmail.com>
2015-06-05 23:00:50 +0530

    Removed all tests and implementaion code

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,32 +1,6 @@
 package com.twu.biblioteca;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BibliotecaApp {
-    List<String[]> books;
-
-    public BibliotecaApp() {
-        books = new ArrayList<>();
-        books.add(new String[]{"Harry Potter and The Sorcer\'s Stone", "J. K. Rowling", "2000"});
-        books.add(new String[]{"Harry Potter and The Chamber of Secrets", "J. K. Rowling", "2001"});
-    }
-
-    public void welcomeMessage() {
-        System.out.println("Welcome to Biblioteca");
-    }
-
-    public void displayBooks() {
-        StringBuilder sb = new StringBuilder();
-        for (String[] book : books) {
-            sb.append(book[0]).append(", ").append(book[1]).append(", ").append(book[2]).append("\n");
-        }
-        System.out.println(sb.toString());
-    }
-
     public static void main(String[] args) {
-        BibliotecaApp bibliotecaApp = new BibliotecaApp();
-        bibliotecaApp.welcomeMessage();
-        bibliotecaApp.displayBooks();
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,47 +1,5 @@
 package com.twu.biblioteca;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertThat;
-
 public class BibliotecaAppTest {
-    private ByteArrayOutputStream outputStream;
 
-    @Before
-    public void setUp() {
-        outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
-    }
-
-    @Test
-    public void shouldBeAbleToDisplayAWelcomeMessageOnAppStart() {
-        BibliotecaApp bibliotecaApp = new BibliotecaApp();
-        bibliotecaApp.welcomeMessage();
-
-        String actualMessage = outputStream.toString();
-
-        assertThat(actualMessage, is("Welcome to Biblioteca\n"));
-    }
-
-    @Test
-    public void shouldBeAbleToDisplayAListOfBooks() {
-        BibliotecaApp bibliotecaApp = new BibliotecaApp();
-        bibliotecaApp.displayBooks();
-
-        String actualListOfBooks = outputStream.toString();
-
-        assertThat(actualListOfBooks, is(not("\n")));
-    }
-
-    @After
-    public void clean() {
-        System.setOut(null);
-    }
 }
