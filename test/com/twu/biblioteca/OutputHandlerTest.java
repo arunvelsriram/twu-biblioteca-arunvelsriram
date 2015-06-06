c1df6eb5a234f2a864b540ecff666f52a2198682
Arunvel Sriram <arunvelsriram@gmail.com>
2015-06-06 16:11:20 +0530

    Added a newline character to pass display book test

--- test/com/twu/biblioteca/OutputHandlerTest.java
+++ test/com/twu/biblioteca/OutputHandlerTest.java
@@ -40,7 +40,7 @@ public class OutputHandlerTest {
         String actualBookDetails = byteArrayOutputStream.toString();
 
         assertThat(actualBookDetails, is(equalTo("| Harry Potter and The Sorcer's Stone | JK Rowling | 1999 |\n" +
-                "| Harry Potter and The Chamber of Secrets | JK Rowling | 2000 |")));
+                "| Harry Potter and The Chamber of Secrets | JK Rowling | 2000 |\n")));
     }
 
     @After
