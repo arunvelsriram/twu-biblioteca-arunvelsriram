afde6a2738b49b32c0fd934fe21724ed49610166
Arunvel Sriram <arunvelsriram@gmail.com>
2015-06-06 09:49:42 +0530

    Passed reflexivity property of equality of books

--- src/com/twu/biblioteca/Books.java
+++ src/com/twu/biblioteca/Books.java
@@ -4,12 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Books {
-    List<String> books;
+    private List<String> books;
 
     public Books() {
         books = new ArrayList<>();
         books.add("Harry Potter and The Sorcer's Stone");
         books.add("Harry Potter and The Chamber of Secrets");
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+
+        Books otherBooks = (Books) object;
+
+        return !(books != null ? !books.equals(otherBooks.books) : otherBooks.books != null);
+    }
+
+    @Override
+    public int hashCode() {
+        return books != null ? books.hashCode() : 0;
     }
 
     @Override
--- test/com/twu/biblioteca/BooksTest.java
+++ test/com/twu/biblioteca/BooksTest.java
@@ -7,14 +7,19 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class BooksTest {
-    private Books books;
-
     @Test
     public void shouldBeAbleToReturnBookDetails() throws Exception {
-        books = new Books();
+        Books books = new Books();
 
         String actualBookDetails = books.toString();
 
         assertThat(actualBookDetails, is(equalTo("Harry Potter and The Sorcer's Stone\nHarry Potter and The Chamber of Secrets\n")));
+    }
+
+    @Test
+    public void equalityShouldSatisfyReflexivity() throws Exception {
+        Books books = new Books();
+
+        assertThat(books, is(equalTo(books)));
     }
 }
