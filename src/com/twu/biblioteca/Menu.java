ea2295d9e89d6de282f24f7bed79027b739f03ff
Arunvel Sriram <arunvelsriram@gmail.com>
2015-06-07 22:27:20 +0530

    Removed Listener Interface and map between manuItem and ActionListener

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -5,19 +5,10 @@ import java.util.Map;
 
 public class Menu {
     private Map<Integer, String> menuItems;
-    private Map<String, MenuItemActionListener> menuItemActionMap;
 
     public Menu() {
         menuItems = new HashMap<>();
-        menuItemActionMap = new HashMap<>();
         menuItems.put(1, "List Books");
-        menuItemActionMap.put("List Books", new MenuItemActionListener() {
-            @Override
-            public void actionPerformed(InputOutputHandler inputOutputHandler) {
-                Books books = new Books();
-                inputOutputHandler.bookDetails(books);
-            }
-        });
     }
 
     @Override
