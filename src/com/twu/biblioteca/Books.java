d5fe0925c90cd6b9e634019a061d53b39e6c275c
Arunvel Sriram <arunvelsriram@gmail.com>
2015-06-07 23:13:49 +0530

    Change List of String[] to List of Book

--- src/com/twu/biblioteca/Books.java
+++ src/com/twu/biblioteca/Books.java
@@ -4,24 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Books {
-    private List<String[]> books;
+    private List<Book> books;
 
     public Books() {
         books = new ArrayList<>();
-        books.add(new String[]{"Harry Potter and The Sorcer's Stone", "JK Rowling", "1999"});
-        books.add(new String[]{"Harry Potter and The Chamber of Secrets", "JK Rowling", "2000"});
+        books.add(new Book("Harry Potter and The Sorcer's Stone", "JK Rowling", 1999));
+        books.add(new Book("Harry Potter and The Chamber of Secrets", "JK Rowling", 2000));
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (String[] bookDetails : books) {
-            sb.append("| ");
-            for(String bookDetail : bookDetails) {
-                sb.append(bookDetail).append(" | ");
-            }
-            sb.deleteCharAt(sb.length() - 1);
-            sb.append("\n");
+        for(Book book : books) {
+            sb.append(book).append("\n");
         }
         sb.deleteCharAt(sb.length() - 1);
         return sb.toString();
