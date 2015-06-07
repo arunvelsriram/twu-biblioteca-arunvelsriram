0c93c38e87c3a34cf628c0f049552a54d528f408
Arunvel Sriram <arunvelsriram@gmail.com>
2015-06-08 02:30:39 +0530

    Passed test for invoking choose method on Menu

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -5,10 +5,24 @@ import java.util.Map;
 
 public class Menu {
     private Map<Integer, String> menuItems;
+    private Map<String, MenuItemActionListener> menuItemActionMap;
 
     public Menu() {
         menuItems = new HashMap<>();
         menuItems.put(1, "List Books");
+    }
+    
+    public Menu(MenuItemActionListener menuItemActionListener) {
+        menuItems = new HashMap<>();
+        menuItemActionMap = new HashMap<>();
+        menuItems.put(1, "List Books");
+        menuItemActionMap.put("List Books", menuItemActionListener);
+    }
+
+    public void choose(int option) {
+        String menuItem = menuItems.get(option);
+        MenuItemActionListener menuItemActionListener = menuItemActionMap.get(menuItem);
+        menuItemActionListener.actionPerformed();
     }
 
     @Override
--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -1,18 +1,40 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+
 public class MenuTest {
+    @Mock
+    private MenuItemActionListenerImpl menuItemActionListenerStub;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     public void shouldBeAbleToReturnAvailableOptions() {
-        Menu menu = new Menu();
+        Menu menu = new Menu(new MenuItemActionListenerImpl());
 
         String actualOptions = menu.toString();
 
         assertThat(actualOptions, is(equalTo("1. List Books\nEnter your choice...")));
+    }
+
+    @Test
+    public void shouldBeAbleToInvokeAnActionForAParticularMenuItem() throws Exception {
+        Menu menu = new Menu(menuItemActionListenerStub);
+//        when(mockFactoryHelper.makeMenuItemActionListener())
+//                .thenReturn(menuItemActionListenerStub);
+        menu.choose(1);
+        Mockito.verify(menuItemActionListenerStub).actionPerformed();
     }
 }
