3d23a8d75d0042968c30e9e9b45cdb01449d73e3
Arunvel Sriram <arunvelsriram@gmail.com>
2015-06-12 01:11:42 +0530

    Added all spec for equality of book

--- src/com/twu/biblioteca/models/Book.java
+++ src/com/twu/biblioteca/models/Book.java
@@ -23,4 +23,20 @@ public class Book {
                 .append(" |");
         return sb.toString();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+
+        Book book = (Book) o;
+
+        return !(title != null ? !title.equals(book.title) : book.title != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return title != null ? title.hashCode() : 0;
+    }
 }
--- test/com/twu/biblioteca/models/BookTest.java
+++ test/com/twu/biblioteca/models/BookTest.java
@@ -1,10 +1,10 @@
 package com.twu.biblioteca.models;
 
-import com.twu.biblioteca.models.Book;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
 public class BookTest {
@@ -16,5 +16,55 @@ public class BookTest {
         String actualBook = book.toString();
 
         assertThat(actualBook, is(equalTo("| Harry Potter and The Sorcer's Stone | JK Rowling | 1999 |")));
+    }
+
+    @Test
+    public void equalityShouldSatisfyReflexivity() {
+        Book bookOne= new Book("Harry Potter an The Sorcer's Stone", "JK Rowling", 1999);
+
+        assertThat(bookOne, is(equalTo(bookOne)));
+    }
+
+    @Test
+    public void equalityShouldSatisfySymmetricity() {
+        Book bookOne = new Book("Harry Potter an The Sorcer's Stone", "JK Rowling", 1999);
+        Book bookTwo = new Book("Harry Potter an The Sorcer's Stone", "JK Rowling", 1999);
+
+        assertThat(bookOne, is(equalTo(bookTwo)));
+        assertThat(bookTwo, is(equalTo(bookOne)));
+    }
+
+    @Test
+    public void equalityShouldSatisfyTransitivity() {
+        Book bookOne = new Book("Harry Potter an The Sorcer's Stone", "JK Rowling", 1999);
+        Book bookTwo = new Book("Harry Potter an The Sorcer's Stone", "JK Rowling", 1999);
+        Book bookThree = new Book("Harry Potter an The Sorcer's Stone", "JK Rowling", 1999);
+
+        assertThat(bookOne, is(equalTo(bookTwo)));
+        assertThat(bookTwo, is(equalTo(bookThree)));
+        assertThat(bookOne, is(equalTo(bookThree)));
+    }
+
+    @Test
+    public void equalityShouldReturnFalseOnPassingNull() {
+        Book bookOne = new Book("Harry Potter an The Sorcer's Stone", "JK Rowling", 1999);
+
+        assertFalse(bookOne.equals(null));
+    }
+
+    @Test
+    public void equalityShouldReturnFalseOnPassingOtherObject() {
+        Book bookOne = new Book("Harry Potter an The Sorcer's Stone", "JK Rowling", 1999);
+
+        assertFalse(bookOne.equals(new String("Hello, World")));
+    }
+
+    @Test
+    public void whenTwoObjectsAreEqualThenTheirHashCodeMustBeEqual() {
+        Book bookOne = new Book("Harry Potter an The Sorcer's Stone", "JK Rowling", 1999);
+        Book bookTwo = new Book("Harry Potter an The Sorcer's Stone", "JK Rowling", 1999);
+
+        assertThat(bookOne, is(equalTo(bookTwo)));
+        assertThat(bookOne.hashCode(), is(equalTo(bookTwo.hashCode())));
     }
 }
