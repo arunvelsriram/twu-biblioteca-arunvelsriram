d10a924f5b90bfcd71b21ba15b3aef9a54fb9098
Arunvel Sriram <arunvelsriram@gmail.com>
2015-06-17 16:34:07 +0530

    Added equals and hashcode method to User

--- src/com/twu/biblioteca/models/User.java
+++ src/com/twu/biblioteca/models/User.java
@@ -20,4 +20,29 @@ public class User {
     public boolean valid(String libraryNumber, char[] password) {
         return libraryNumber.equals(this.libraryNumber) && Arrays.equals(password, this.password);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (!libraryNumber.equals(user.libraryNumber)) return false;
+        if (!Arrays.equals(password, user.password)) return false;
+        if (!name.equals(user.name)) return false;
+        if (!emailAddress.equals(user.emailAddress)) return false;
+        return phoneNumber.equals(user.phoneNumber);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = libraryNumber.hashCode();
+        result = 31 * result + Arrays.hashCode(password);
+        result = 31 * result + name.hashCode();
+        result = 31 * result + emailAddress.hashCode();
+        result = 31 * result + phoneNumber.hashCode();
+        return result;
+    }
 }
--- test/com/twu/biblioteca/models/UserTest.java
+++ test/com/twu/biblioteca/models/UserTest.java
@@ -3,8 +3,9 @@ package com.twu.biblioteca.models;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 
 public class UserTest {
 
@@ -41,5 +42,55 @@ public class UserTest {
         boolean actual = user.valid("B1111", "pasword".toCharArray());
 
         assertFalse(actual);
+    }
+
+    @Test
+    public void equalityShouldSatisfyReflexivity() {
+        User user = new User("B1011", "passwd".toCharArray(), "Murali", "murali123@gmail.com", "919976767676");
+
+        assertThat(user, is(equalTo(user)));
+    }
+
+    @Test
+    public void equalityShouldSatisfySymmetricity() {
+        User user = new User("B1011", "passwd".toCharArray(), "Murali", "murali123@gmail.com", "919976767676");
+        User userTwo = new User("B1011", "passwd".toCharArray(), "Murali", "murali123@gmail.com", "919976767676");
+
+        assertThat(user, is(equalTo(userTwo)));
+        assertThat(userTwo, is(equalTo(user)));
+    }
+
+    @Test
+    public void equalityShouldSatisfyTransitivity() {
+        User user = new User("B1011", "passwd".toCharArray(), "Murali", "murali123@gmail.com", "919976767676");
+        User userTwo = new User("B1011", "passwd".toCharArray(), "Murali", "murali123@gmail.com", "919976767676");
+        User userThree = new User("B1011", "passwd".toCharArray(), "Murali", "murali123@gmail.com", "919976767676");
+
+        assertThat(user, is(equalTo(userTwo)));
+        assertThat(userTwo, is(equalTo(userThree)));
+        assertThat(user, is(equalTo(userThree)));
+    }
+
+    @Test
+    public void equalityShouldReturnFalseOnPassingNull() {
+        User user = new User("B1011", "passwd".toCharArray(), "Murali", "murali123@gmail.com", "919976767676");
+
+        assertFalse(user.equals(null));
+    }
+
+    @Test
+    public void equalityShouldReturnFalseOnPassingOtherObject() {
+        User user = new User("B1011", "passwd".toCharArray(), "Murali", "murali123@gmail.com", "919976767676");
+
+        assertFalse(user.equals(new String("Hello, World")));
+    }
+
+    @Test
+    public void whenTwoObjectsAreEqualThenTheirHashCodeMustBeEqual() {
+        User user = new User("B1011", "passwd".toCharArray(), "Murali", "murali123@gmail.com", "919976767676");
+        User userTwo = new User("B1011", "passwd".toCharArray(), "Murali", "murali123@gmail.com", "919976767676");
+
+        assertThat(user, is(equalTo(userTwo)));
+        assertThat(user.hashCode(), is(equalTo(userTwo.hashCode())));
     }
 }
